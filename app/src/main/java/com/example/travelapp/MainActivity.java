bfaa79fe87b1cb547bf873b5af4a1b25b7874892
HadyAhmed00 <61879050+HadyAhmed00@users.noreply.github.com>
2021-08-12 16:20:24 +0200

    Adding a welcome page

--- app/src/main/java/com/example/travelapp/MainActivity.java
+++ app/src/main/java/com/example/travelapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +15,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button b1 = findViewById(R.id.btn1);
+        TextView sin_Up = findViewById(R.id.signup_tv);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(),SignIn.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+        sin_Up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),SignUp.class);
                 startActivityForResult(intent, 0);
             }
         });
