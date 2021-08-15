6c7c9ec0dd9bb2180b7225643d83bcc54cc0e787
Fares Ahmed <faresahmed3993@gmil.com>
2021-08-15 23:17:27 +0200

    menu

--- app/src/main/java/com/example/travelapp/Destination.java
+++ app/src/main/java/com/example/travelapp/Destination.java
@@ -1,9 +1,12 @@
 package com.example.travelapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -40,5 +43,12 @@ public class Destination extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    public boolean onCreatePanelMenu(int featureId, @NonNull Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu1,menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/travelapp/Home.java
+++ app/src/main/java/com/example/travelapp/Home.java
@@ -18,6 +18,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -74,5 +76,13 @@ public class Home extends AppCompatActivity {
 
             }
         });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater =getMenuInflater();
+        inflater.inflate(R.menu.menu1,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
