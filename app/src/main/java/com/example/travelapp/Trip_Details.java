eb79bca8b459e8e3b2d6325598aa6244bde71a54
Fares Ahmed <faresahmed3993@gmil.com>
2021-08-17 13:46:10 +0200

    menu_in_tripDetails

--- app/src/main/java/com/example/travelapp/Trip_Details.java
+++ app/src/main/java/com/example/travelapp/Trip_Details.java
@@ -1,11 +1,18 @@
 package com.example.travelapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Trip_Details extends AppCompatActivity {
     static String from, to, time;
@@ -40,5 +47,32 @@ public class Trip_Details extends AppCompatActivity {
         to_txt.setText(to);
         price_txt.setText(String.valueOf(price));
         time_txt.setText(time);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater =getMenuInflater();
+        inflater.inflate(R.menu.menu1,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Intent intent=new Intent(getApplication(),Profile.class);
+                startActivity(intent);
+                return true;
+            case R.id.item2:
+                Intent intent1=new Intent(getApplication(),About.class);
+                startActivity(intent1);
+                return true;
+            case R.id.item3:
+                Intent intentt=new Intent(getApplication(),MainActivity.class);
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                startActivity(intentt);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
